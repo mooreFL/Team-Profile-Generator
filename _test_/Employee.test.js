09cf9f8244844e7176f88cb7f13de0387b88fdb5
mooreFL <moorepr09@gmail.com>
2021-04-16 18:59:37 -0400

    added manager questions

--- _test_/Employee.test.js
+++ _test_/Employee.test.js
@@ -1,7 +1,28 @@
 const { expect } = require("@jest/globals");
-const Employee = require("../lib/Employees");
+const Employee = require("../lib/Employee");
 
 test ("Can instantiate Employee", () => {
     const employee = new Employee();
     expect(typeof(employee)).toBe("object");
 })
+
+test ("Can set name through constructor arguements", () => {
+    const employee = new Employee("Patrick");
+    expect(employee.name).toBe("Patrick");
+})
+
+test ("Can get name via getName()", () => {
+    const employee = new Employee("Patrick");
+    expect(employee.getName()).toBe("Patrick");
+})
+
+//add test for id
+
+// add test for email
+
+// add test for getid
+
+// add test for getEmail
+
+// add test for getRole
+
--- index.js
+++ index.js
@@ -1,6 +1,81 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
-const Employee = require("./lib/Employees");
 const Intern = require("./lib/Intern");
-const inquire = require("inquirer");
+const path = require("path");
 const fs = require("fs");
+const inquirer = require("inquirer");
+const teamArray = [];
+
+// first function to create manager, run through all manager questions. When done run .then; create the new manager. When finished call the create team function
+
+function createManager() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Enter the manager's name",
+        validate: (data) => {
+          if (data !== "") {
+            return true;
+          }
+          return "Please enter a name.";
+        },
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is the manager's id?",
+        validate: (data) => {
+          if (data !== "") {
+            return true;
+          }
+          return "Please enter an ID.";
+        },
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is the manager's email?",
+        validate: (data) => {
+          if (data !== "") {
+            return true;
+          }
+          return "Please enter an email.";
+        },
+      },
+      {
+        type: "input",
+        name: "officeNumber",
+        message: "What is the manager's office number?",
+        validate: (data) => {
+          if (data !== "") {
+            return true;
+          }
+          return "Please enter a number.";
+        },
+      },
+    ])
+    .then((data) => {
+      const manager = new Manager(
+        data.managerName,
+        data.managerId,
+        data.managerEmail,
+        data.managerOfficeNumber
+      );
+      teamArray.push(manager);
+      createTeam();
+    });
+}
+
+//createteam: will have one question (list) with three options, I.E. Intern, Engineer, Finish building my team
+
+//based on answers you will have a .then, run a switch case
+//i.e.: if engineer then run engineer function
+//if "finish building team" is selected then create everything that was selected
+
+// two more functions exactly like engineer but for manager and intern
+// at end of both call create team function
+
+//push the created to the array that will end up in the html
+// the last function will pass the array to the function that renders the html
