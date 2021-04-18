538a49eb54b2a2ab477dbbf9ab78af8e439bffa1
mooreFL <moorepr09@gmail.com>
2021-04-18 18:05:04 -0400

    manager test issues

--- _test_/Manager.test.js
+++ _test_/Manager.test.js
@@ -1,18 +1,20 @@
 const { expect } = require("@jest/globals");
-const Manager = require("../lib/Employee");
-
+const Manager = require("../lib/Manager")
 describe("Manager", () => {
     describe("Extend", () => {
-        it ("Should create a new project with office number", () =>{
-            const manager = new Manager("A123");
-            expect(manager).toEqual("A123");
-        })
+        it("Should create a new project with office number", () => {
+            const manager = new Manager("Patrick", 123, "moore@gmail.com","A123");
+            expect(manager).toEqual({name: "Patrick", id: 123, email: "moore@gmail.com", officeNumber: "A123"});
+        });
+    });
+
+    test ("Can get office number with officeNumber function", () => {
+        const manager = new Manager("A123");
+        expect(manager.getOfficeNumber()).toBe(manager.officeNumber);
     })
-    test ("get role with getRole function", () => {
-        const manager = new Manager("Patrick");
-        expect(manager.getRole()).toBe('Manager')
-    })
-    test ("get office number with officeNumber function", () => {
-        const manager = new Manager("A123")
+
+    test ("Can get role with getRole function", () => {
+        const manager = new Manager("Manager");
+        expect(manager.getRole()).toBe("Manager");
     })
 })
