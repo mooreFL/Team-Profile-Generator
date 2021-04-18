4cf79837e6468068ca8ddc5e44c2125110001cb8
mooreFL <moorepr09@gmail.com>
2021-04-18 18:30:59 -0400

    added engineer test

---
+++ _test_/Engineer.test.js
@@ -0,0 +1,21 @@
+const { expect } = require("@jest/globals");
+const Engineer = require("../lib/Engineer");
+
+describe("Engineer", () => {
+    describe("Extend", () => {
+        it("Should add engineer with a school value", () => {
+            const engineer = new Engineer("Patrick", 123, "moore@gmail.com","mooreFL");
+            expect(engineer).toEqual({name: "Patrick", id: 123, email: "moore@gmail.com", gitHub: "mooreFL"});
+        });
+    });
+
+    test ("Can get gitHub username via getGitHub function", () => {
+        const engineer = new Engineer("");
+        expect(engineer.getGitHub()).toBe(engineer.gitHub);
+    })
+
+    test ("Can get role with getRole function", () => {
+        const engineer = new Engineer("Engineer");
+        expect(engineer.getRole()).toBe("Engineer");
+    })
+})
